c0435ab49e23312c1d0be2339dd545ed35254ee1
Lukas Oppermann <oppermann.lukas@gmail.com>
2018-04-09 10:09:51 +0200

    now getting name of artboard

--- src/browser-preview.js
+++ src/browser-preview.js
@@ -8,26 +8,36 @@ export default function(context) {
   const document = sketch.getSelectedDocument()
   // get selected page
   const page = document.selectedPage
-  // get selected artboard
-  if (context.selection.count() == 0) {
+  
+  // if no artboard selected
+  if (context.selection.length == 0) {
     context.document.showMessage('⚠️ Please select an artboard.');
     return;
   }
+  
+  if( context.selection.length === 1 ){
+    const artboard = context.selection.firstObject();
+    if( artboard && artboard.isKindOfClass(MSArtboardGroup) ){
+      context.document.showMessage(artboard.name())
+    }
+    return;
+  }
+  
 
-    let selectedArtboards = new Set(
-      util.arrayFromNSArray(context.selection)
-        .map(layer => util.getContainingArtboard(layer))
-        .filter(layer => !!layer)
-    );
-    
-    
-    
-    selectedArtboards.forEach(artboard => {
-        context.document.showMessage(artboard.name);
-    })
-    
-    
-    util.setSelection(context, Array.from(selectedArtboards));
+    // let selectedArtboards = new Set(
+    //   util.arrayFromNSArray(context.selection)
+    //     .map(layer => util.getContainingArtboard(layer))
+    //     .filter(layer => !!layer)
+    // );
+    // 
+    // 
+    // 
+    // selectedArtboards.forEach(artboard => {
+    //     context.document.showMessage(artboard.name);
+    // })
+    // 
+    // 
+    // util.setSelection(context, Array.from(selectedArtboards));
 
 
   // if (artboard.length === 0) {
